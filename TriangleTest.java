9a06a78483693c2a8b642d7dd9842e9b69920591
Sanpath Sunggad <22544750+sanpath18@users.noreply.github.com>
2018-03-03 00:03:36 +0700

    TriangleTest.java
    Add more test

--- TriangleTest.java
+++ TriangleTest.java
@@ -8,8 +8,39 @@ public class TriangleTest {
 
     @Test
     void testEquilateral() {
-        TriangleType type = Triangle.classify(1, 1, 1);
+        TriangleType type = Triangle.classify(12, 12, 12);
         assertEquals(TriangleType.EQUILATERAL, type);
+    }
+
+    @Test
+    void testScalene() {
+        TriangleType type = Triangle.classify(7, 15, 12);
+        assertEquals(TriangleType.SCALENE, type);
+    }
+
+    @Test
+        //(trian == 1) && ((a + b) > c)
+    void testIsosceles1() {
+        TriangleType type = Triangle.classify(9, 9, 15);
+        assertEquals(TriangleType.ISOSCELES, type);
+    }
+    @Test
+        //(trian == 2) && ((a + c) > b)
+    void testIsosceles2() {
+        TriangleType type = Triangle.classify(9, 15, 9);
+        assertEquals(TriangleType.ISOSCELES, type);
+    }
+    @Test
+        //(trian == 3) && ((b + c) > a)
+    void testIsosceles3() {
+        TriangleType type = Triangle.classify(9, 15, 15);
+        assertEquals(TriangleType.ISOSCELES, type);
+    }
+
+    @Test
+    void testInvalid() {
+        TriangleType type = Triangle.classify(-9, 15, 9);
+        assertEquals(TriangleType.INVALID, type);
     }
 
 }
